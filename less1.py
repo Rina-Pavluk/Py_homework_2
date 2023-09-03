2d0fb357412e5c38587811f8354a83c499775849
Ekaterina Pavluk <rinapalijlinnik@gmail.com>
2023-09-03 23:04:46 +0300

    less1

--- less1.py
+++ less1.py
@@ -1,1 +1,17 @@
-monet = ('')
+
+# coins = input("Введите монеты где 0 - это решка, а 1 - это герб:")
+
+# zero = 0
+# one = 0
+
+# for number in coins:
+#     if number == "0":
+#         zero += 1
+#     elif number == '1':
+#         one += 1
+
+# mincount = min(zero , one)
+
+# print(f"Минимальное колл-во монет,которое нужно перевернуть {mincount}")
+
+
